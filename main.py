1d252e0c8adf7db73d249ba75885bf4dbff54f4a
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-03-13 12:22:00 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,26 @@
+Explorer = sprites.create(img("""
+        . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+guess = game.ask_for_number("Guess the number")
+number2 = randint(0, 10)
+output_text = "That is wrong! it was " + str(number2)
+if number2 == guess:
+    Explorer.say_text("You guessed it!")
+else:
+    game.splash(output_text)
